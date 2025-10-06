ba153b898497b2bf57c6341759dfb4916eb5da39
Tarcan Gul <nailtarcan@gmail.com>
2025-10-05 21:29:50 -0400

    feat: add knob dynamically

--- src/PluginEditor.cpp
+++ src/PluginEditor.cpp
@@ -16,13 +16,21 @@ CaveyAudioProcessorEditor::CaveyAudioProcessorEditor(CaveyAudioProcessor& p)
     promptEditor.setTextToShowWhenEmpty(PROMPT_PLACEHOLDER_TEXT, Colours::grey.withAlpha(.6f));
 
     generateButton.setButtonText(GENERATE_BUTTON_TEXT);
+    generateButton.addListener(this);
 
     addAndMakeVisible(&mainLabel);
     addAndMakeVisible(&promptEditor);
     addAndMakeVisible(&generateButton);
 }
 
-CaveyAudioProcessorEditor::~CaveyAudioProcessorEditor() = default;
+CaveyAudioProcessorEditor::~CaveyAudioProcessorEditor() {
+    generateButton.removeListener(this);
+    if (!parameterKnobs.empty()) {
+        for (auto it = parameterKnobs.begin(); it != parameterKnobs.end(); it++) {
+            delete *it;
+        }
+    }
+}
 
 void CaveyAudioProcessorEditor::paint(juce::Graphics& g)
 {
@@ -33,9 +41,47 @@ void CaveyAudioProcessorEditor::resized() {
     auto screen = getLocalBounds();
     auto promptBounds = screen.removeFromBottom(PROMPT_HEIGHT);
     auto buttonBounds = promptBounds.removeFromRight(PROMPT_WIDTH);
+
+    if (parameterKnobs.empty()) {
+        mainLabel.setVisible(true);
+        mainLabel.setBounds(screen.withSizeKeepingCentre(mainLabel.getFont().getStringWidth(MAIN_LABEL_TEXT), 20));
+    } else {
+        for (auto it = parameterKnobs.begin(); it != parameterKnobs.end(); it++) {
+            Slider * knob = *it;
+            knob->setBounds(screen.removeFromTop(KNOB_INIT_Y_POS + KNOB_HEIGHT * std::distance(parameterKnobs.begin(), it)));
+        }
+    }
+
     // Divide the screen to four areas (header - main area - text area - footer)
-    mainLabel.setBounds(screen.withSizeKeepingCentre(mainLabel.getFont().getStringWidth(MAIN_LABEL_TEXT), 20));
     promptEditor.setBounds(promptBounds.reduced(MARGIN_SMALL));
     generateButton.setBounds(buttonBounds.reduced(MARGIN_EXTRA_SMALL, MARGIN_SMALL));
+}
+
+void CaveyAudioProcessorEditor::buttonClicked(juce::Button *buttonRef) {
+    if (buttonRef == &generateButton) {
+        whenGenerateButtonClicked();
+    }
+}
+
+void CaveyAudioProcessorEditor::whenGenerateButtonClicked() {
+    // Generate the knob here.
+    if (parameterKnobs.size() < MAX_PARAMETER_AMOUNT) {
+        Slider * slider = new Slider();
+        slider->setSliderStyle(juce::Slider::SliderStyle::Rotary);
+        slider->setRange(0, 100, 1);
+        addAndMakeVisible(slider);
+        parameterKnobs.push_back(slider);
+        parameterAdded();
+    }
+}
+
+void CaveyAudioProcessorEditor::parameterAdded() {
+    PRINT("Parameter added");
+    auto screen = getLocalBounds();
+    for (auto it = parameterKnobs.begin(); it != parameterKnobs.end(); it++) {
+        Slider * knob = *it;
+        knob->setBounds(screen.removeFromTop(KNOB_INIT_Y_POS + KNOB_HEIGHT * std::distance(parameterKnobs.begin(), it)));
+    }
+    mainLabel.setVisible(false);
 }
 
--- src/PluginEditor.h
+++ src/PluginEditor.h
@@ -1,6 +1,10 @@
 #pragma once
 
 #include <JuceHeader.h>
+#include <vector>
+#include <iterator>
+
+#define PRINT(text) std::cout << text << std::endl
 
 static constexpr const int MARGIN_EXTRA_SMALL = 10;
 static constexpr const int MARGIN_SMALL = 20;
@@ -8,15 +12,20 @@ static constexpr const int PROMPT_WIDTH = 100;
 static constexpr const int PROMPT_HEIGHT = 200;
 static constexpr int INIT_SCREEN_WIDTH = 600;
 static constexpr int INIT_SCREEN_HEIGHT = 600;
+static constexpr int MAX_PARAMETER_AMOUNT = 3;
 
 // Text
 static constexpr const char *const MAIN_LABEL_TEXT = "Welcome to Cavey! Start with a prompt to generate a parameter.";
 static constexpr const char *const PROMPT_PLACEHOLDER_TEXT = "Write your prompt here.";
 static constexpr const char *const GENERATE_BUTTON_TEXT = "Generate";
 
+static constexpr const int KNOB_INIT_Y_POS = 100;
+
+static constexpr const int KNOB_HEIGHT = 10;
+
 class CaveyAudioProcessor;
 
-class CaveyAudioProcessorEditor : public juce::AudioProcessorEditor {
+class CaveyAudioProcessorEditor : public juce::AudioProcessorEditor, public juce::Button::Listener {
 public:
     explicit CaveyAudioProcessorEditor(CaveyAudioProcessor&);
     ~CaveyAudioProcessorEditor() override;
@@ -25,10 +34,15 @@ public:
     void resized() override;
 
 private:
+    void buttonClicked(Button * buttonRef) override;
+    void whenGenerateButtonClicked();
+
+    void parameterAdded();
     CaveyAudioProcessor& audioProcessor;
     Label mainLabel;
     TextEditor promptEditor;
     TextButton generateButton;
+    std::vector<Slider *> parameterKnobs = {};
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(CaveyAudioProcessorEditor)
 };
