11a3330630627dc43470122ce565c49c0e36f7d1
Tarcan Gul <nailtarcan@gmail.com>
2026-01-19 11:20:22 -0500

    refactor: keep the same flow but do async

---
+++ src/components/LoadingComponent.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Tarcan Gul on 1/19/26.
+//
+
+#include "LoadingComponent.h"
---
+++ src/components/LoadingComponent.h
@@ -0,0 +1,16 @@
+//
+// Created by Tarcan Gul on 1/19/26.
+//
+
+#ifndef CAVEYPLUGIN_LOADINGCOMPONENT_H
+#define CAVEYPLUGIN_LOADINGCOMPONENT_H
+
+
+
+class LoadingComponent {
+
+};
+
+
+
+#endif //CAVEYPLUGIN_LOADINGCOMPONENT_H
